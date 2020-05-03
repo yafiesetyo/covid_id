42d27fca8acd784e4926f714a1b82ba69cfe127a
satriaamien <satriaamien0@gmail.com>
2020-05-04 00:42:22 +0700

    tampilan awal lumayan joss

--- js/home_script.js
+++ js/home_script.js
@@ -1,37 +1,21 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawPie);
 
+
 function drawPie(){
-
-    var jsonData = $.ajax({
-        url : 'https://api.kawalcorona.com/indonesia/provinsi/',
-        method : 'GET',
-        dataType : 'JSON',
-        async : false
-    });
-
-    jsonData = jsonData.responseJSON;
-
-    var others = 0;
-    for (i=4; i<jsonData.length;i++){
-        others += jsonData[i].attributes.Kasus_Posi;
-    }
-
     var data = google.visualization.arrayToDataTable([
-        ['Provinsi','Jumlah Positif'],
-        [jsonData[0].attributes.Provinsi,jsonData[0].attributes.Kasus_Posi],
-        [jsonData[1].attributes.Provinsi,jsonData[1].attributes.Kasus_Posi],
-        [jsonData[2].attributes.Provinsi,jsonData[2].attributes.Kasus_Posi],
-        [jsonData[3].attributes.Provinsi,jsonData[3].attributes.Kasus_Posi],
-        [jsonData[4].attributes.Provinsi,jsonData[4].attributes.Kasus_Posi],
-        ['lainnya',others]
+        ['Task', 'Hours per Day'],
+        ['Work', 8],
+        ['Friends', 2],
+        ['Eat', 2],
+        ['TV', 2],
+        ['Gym', 2],
+        ['Sleep', 8]
     ]);
-    // var options ={
-    //     is3D:true
-    // }
+    var options ={
+        legend : 'none',
+        is3D:true
+    }
     var chart = new google.visualization.PieChart(document.getElementById('pie'));
-    chart.draw(data);
+    chart.draw(data,options);
 }
-
-
-
